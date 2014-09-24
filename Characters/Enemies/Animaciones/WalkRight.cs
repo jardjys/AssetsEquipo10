e5d6946b2b733dde953bf9d6b61883192a589f0b
jardjys <jardjys@hotmail.com>
2014-09-24 14:44:45 -0500

    Animaciones y Assets

---
+++ Characters/Enemies/Animaciones/WalkRight.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkRight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.D)) {
+			Vector3 PosActual = transform.position;
+			PosActual = PosActual + new Vector3 (1, 0, 0) * Time.deltaTime;
+			transform.position = PosActual;
+		}
+	}
+}
